0b952b005637ee67f56286a1831ad90a482a8751
charise <charise.r.wills@gmail.com>
2023-03-22 19:47:52 +1000

    Project reworked into SCSS. Kanji page is completed. Responsivity working on kanji page, particularly with regards to back to home button and stroke selector bar. Class names edited to match BEM conventions

---
+++ javascript/script.js
@@ -0,0 +1,38 @@
+document.getElementById("adjectivesButton").onclick = function () {
+	window.location.href = "html/Adjectives.html";
+};
+document.getElementById("adverbsButton").onclick = function () {
+	window.location.href = "html/Adverbs.html";
+};
+document.getElementById("conjunctionsButton").onclick = function () {
+	window.location.href = "html/Conjunctions.html";
+};
+document.getElementById("comparisonsButton").onclick = function () {
+	window.location.href = "html/Comparisons.html";
+};
+document.getElementById("demonstrativesButton").onclick =
+	function () {
+		window.location.href = "html/Demonstratives.html";
+	};
+document.getElementById("interjectionsButton").onclick = function () {
+	window.location.href = "html/Interjections.html";
+};
+document.getElementById("interrogativesButton").onclick =
+	function () {
+		window.location.href = "html/Interrogatives.html";
+	};
+document.getElementById("nounsButton").onclick = function () {
+	window.location.href = "html/Nouns.html";
+};
+document.getElementById("particlesButton").onclick = function () {
+	window.location.href = "html/Particles.html";
+};
+document.getElementById("verbsButton").onclick = function () {
+	window.location.href = "html/Verbs.html";
+};
+document.getElementById("kanjiButton").onclick = function () {
+	window.location.href = "html/Kanji.html";
+};
+document.getElementById("expressionsButton").onclick = function () {
+	window.location.href = "html/Expressions.html";
+};
